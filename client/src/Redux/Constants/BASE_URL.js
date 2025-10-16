995b7a8e51b969d9c3900b4a1b442084eb18d063
Marco-Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2025-10-16 16:03:43 -0300

    changue url backend

--- client/src/Redux/Constants/BASE_URL.js
+++ client/src/Redux/Constants/BASE_URL.js
@@ -1,3 +1,2 @@
 // export const BASE_URL = "http://localhost:4000";
-export const BASE_URL =
-  "https://tienda-digital-qrq8bugvg-marcos-projects-0dd3bd3c.vercel.app";
+export const BASE_URL = "https://tienda-digital-one.vercel.app";
